22bb4469b60ab7168633f6957fb4450f062614e9
Michal Lewandowski <michalewy00@gmail.com>
2023-10-19 21:22:27 +0200

    add total number of produtcs

--- Zadanie-git.py
+++ Zadanie-git.py
@@ -10,3 +10,6 @@ for sklep, produkty in Lista_zakupow.items():
     sklep = sklep.capitalize()
     produkty = [produkt.capitalize() for produkt in produkty]
     print(f"idę do {sklep} i kupuje {produkty}")
+    #Sposób policzenia produktów 
+Liczba_produktów = len(Lista_zakupow["piekarnia"]+Lista_zakupow["warzywniak"])
+print(f"W sumie kupuję {Liczba_produktów} produktów")
