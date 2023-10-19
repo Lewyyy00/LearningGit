e20a25078ce1974d8879dab522386214daaee237
Michal Lewandowski <michalewy00@gmail.com>
2023-10-19 21:19:52 +0200

    add loop for

--- Zadanie-git.py
+++ Zadanie-git.py
@@ -4,3 +4,9 @@ Lista_zakupow = {
     "piekarnia":["chleb", "bułki", "pączek"],
     "warzywniak":["marchew", "seler", "rukola"]
 }
+print("Lista zakupów")
+#Tworzenie pętli 
+for sklep, produkty in Lista_zakupow.items():
+    sklep = sklep.capitalize()
+    produkty = [produkt.capitalize() for produkt in produkty]
+    print(f"idę do {sklep} i kupuje {produkty}")
